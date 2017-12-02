da746c1ef8ecee69db4e2bc150b0a1db2fb4a670
Michael McCormick <michael4.mccormick@live.uwe.ac.uk>
2017-12-02 17:11:39 +0000

    System call switch statement setup, and SYS_HALT implemented

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -12,10 +12,22 @@ syscall_init (void)
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-
 static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
-  printf ("system call!\n");
-  thread_exit ();
+  /* Get the system call number,
+  which is define in 'Pintos/lib/syscall-nr.h' */
+  uint32_t *p = f->esp;
+
+  //Switch statement for handling system calls
+  switch(*p) {
+
+    // Case for System Halt called
+    case SYS_HALT:{
+      printf("System HALT has been called!\n");
+      shutdown();
+      break;
+    }
+
+  }
 }
