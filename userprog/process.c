3ac98f21ebd7937077923fb2ed5fb25510d0154f
Michael McCormick <michael4.mccormick@live.uwe.ac.uk>
2017-12-02 16:07:25 +0000

    Argument passing termination processing error to be fixed

--- userprog/process.c
+++ userprog/process.c
@@ -108,6 +108,9 @@ process_exit (void)
   struct thread *cur = thread_current ();
   uint32_t *pd;
 
+  // Handles termination messaging
+  printf("%s : exit(%d)\n", cur->name, cur->exit_code);
+
   /* Destroy the current process's page directory and switch back
      to the kernel-only page directory. */
   pd = cur->pagedir;
