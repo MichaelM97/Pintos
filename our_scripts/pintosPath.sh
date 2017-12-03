827399893fb4a6741b71a75393e69b5b1c35eca8
Dominic Lewis <dominic2.lewis@live.uwe.ac.uk>
2017-12-03 20:24:51 +0000

    Added some scripts if pintos path isn't defaultly set up

---
+++ our_scripts/pintosPath.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+export PATH=$PATH:/home/dominic/Pintos/utils
+echo "Type: printenv PATH"
+echo "If PATH is not set to pintos directory then run the script like so"
+echo ". ./script_name"
